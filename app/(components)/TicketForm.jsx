f0a802df8d080908b82478fb7af4ba89583e8351
Ben Aitcheson <benaitcheson@hotmail.com>
2024-09-11 08:41:02 +1000

    ADD: ticketform inputs

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,134 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = () => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/route", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+    });
+    if (res.ok) {
+      router.push("/");
+    }
+  };
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "open",
+    category: "bug",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+
+  const priorities = [
+    { value: 1, label: 'High' },
+    { value: 2, label: 'Medium-High' },
+    { value: 3, label: 'Medium' },
+    { value: 4, label: 'Low' }
+  ];
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>Create a new ticket</h3>
+        <label htmlFor="title">Title: </label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+
+        <label htmlFor="description">Description: </label>
+        <textarea
+          id="description"
+          name="description"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows={4}
+        />
+
+        <label htmlFor="category">Category: </label>
+        <select
+          name="category"
+          onChange={handleChange}
+          value={formData.description}
+        >
+          <option value="bug">Bug</option>
+          <option value="feature">Feature</option>
+          <option value="chore">Other</option>
+        </select>
+
+        <label htmlFor="priority">Priority: </label>
+        <div>
+          {priorities.map((priority) => (
+            <div key={priority.value}>
+              <input
+                id={`priority-${priority.value}`}
+                name="priority"
+                type="radio"
+                onChange={handleChange}
+                required={true}
+                value={priority.value}
+                checked={formData.priority == priority.value}
+              />
+              <label htmlFor={`priority-${priority.value}`}>{priority.label}</label>
+            </div>
+          ))}
+        </div>
+
+        <label htmlFor="progress">Progress: </label>
+        <input
+          id="progress"
+          name="progress"
+          type="range"
+          onChange={handleChange}
+          value={formData.progress}
+          min={0}
+          max={100}
+        />
+
+        <label htmlFor="status">Status: </label>
+        <select
+          name="status"
+          onChange={handleChange}
+          value={formData.status}
+        >
+          <option value="open">Open</option>
+          <option value="in-progress">In Progress</option>
+          <option value="resolved">Resolved</option>
+          <option value="in-review">Code Review</option>
+          <option value="requires-further-information">Requires Further Information</option>
+          <option value="closed">Closed</option>
+        </select>
+
+        <input type="submit" className="btn" value="Create Ticket" />
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
---
+++ app/api/route.js
@@ -0,0 +1,15 @@
+import Ticket from "../(models)/Ticket";
+import { NextResponse } from "next";
+
+export default async function POST(req) {
+  try {
+    const body = await req.json();
+    const ticketData = body.formData;
+    await Ticket.create(ticketData);
+
+    return NextResponse.json({ message: "Ticket created" }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
+  }
+}
